7a6c06da9cf6f15ffc99d89b0a32574d9003f8ad
F1End <horvirich@gmail.com>
2024-12-24 12:30:56 +0100

    Initial commit with file system drawn out

---
+++ src/nudge.py
@@ -0,0 +1,5 @@
+# import ctypes
+#
+# msg = "This is a test msg!$2"
+# title = "Test title here!"
+# ctypes.windll.user32.MessageBoxW(0, msg, title, 0)
---
+++ src/watch.py
@@ -0,0 +1,6 @@
+import wmi
+#
+# c = wmi.WMI()
+# print("pid    Process name")
+# for process in c.Win32_Process():
+#     print(f"{process.ProcessId:<10} {process.Name}")
